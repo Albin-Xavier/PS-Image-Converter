e8af63b0194b923d5df47fef0c28afc28c3271cf
Albin-Xavier <Albin-Xavier@users.noreply.github.com>
2022-05-21 17:23:57 -0500

    Add files via upload

---
+++ Convert Images to PNG.ps1
@@ -0,0 +1,21 @@
+Set-StrictMode -Version 3.0
+Get-ChildItem -File | Rename-Item -NewName { $_.name -replace "\.(jfif|jpeg)$", ".jpg" }
+# mogrify.exe will throw an error for any file types not found in the directory. As far as I can tell, these errors are benign.
+if (Test-Path -Path .\ImageMagick\mogrify.exe -Pathtype Leaf) {
+	If ($Null -eq (Get-ChildItem -Force .\Input)) {
+		"No images to convert!"
+		Read-Host "Press enter to continue..."
+	}
+	else {
+	$Path=[System.IO.Path]::Combine($PWD.Path, 'Input'); [System.IO.Directory]::CreateDirectory($Path) | Out-Null
+	$Path=[System.IO.Path]::Combine($PWD.Path, 'Output'); [System.IO.Directory]::CreateDirectory($Path) | Out-Null
+	$Path=[System.IO.Path]::Combine($PWD.Path, 'Originals'); [System.IO.Directory]::CreateDirectory($Path) | Out-Null
+	.\ImageMagick\mogrify.exe -path .\Output -format png Input\*.webp Input\*.jpg Input\*.heic Input\*.tiff
+	move-item -Force Input\*.webp, Input\*.jpg, Input\*.heic, Input\*.tiff -Destination .\Originals\
+	}
+}
+
+else {
+	Write-Host "mogrify.exe not found!"
+	Read-Host "Press enter to continue..."
+}
