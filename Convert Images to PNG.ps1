6e7f144a9cd0a6ba77433d3bb1978be57c22426b
Albin-Xavier <Albin-Xavier@users.noreply.github.com>
2022-06-04 11:58:49 -0500

    Automatically setup ImageMagick, general cleanup.

--- Convert Images to PNG.ps1
+++ Convert Images to PNG.ps1
@@ -1,21 +1,22 @@
 Set-StrictMode -Version 3.0
 Get-ChildItem -File | Rename-Item -NewName { $_.name -replace "\.(jfif|jpeg)$", ".jpg" }
 # mogrify.exe will throw an error for any file types not found in the directory. As far as I can tell, these errors are benign.
-if (Test-Path -Path .\ImageMagick\mogrify.exe -Pathtype Leaf) {
-	If ($Null -eq (Get-ChildItem -Force .\Input)) {
-		"No images to convert!"
-		Read-Host "Press enter to continue..."
-	}
-	else {
+if(-not (Test-Path -Path $PSScriptRoot\ImageMagick\magick.exe -Pathtype Leaf)){ 
+	New-Item $PSScriptRoot\ImageMagick -ItemType Directory | Out-Null
+	winget install -e --id ImageMagick.ImageMagick --location $PSScriptRoot\ImageMagick
+}
+if(-not (Test-Path -Path $PSScriptRoot\Input -Pathtype Leaf)){
+	New-Item $PSScriptRoot\Input -ItemType Directory | Out-Null
+	"Input Folder Created! Place your images inside."
+	Read-Host "Press enter to continue..."
+}
+If ($Null -eq (Get-ChildItem -Force .\Input)) {
+	"No images to convert!"
+	Read-Host "Press enter to exit"
+	Exit
+}
 	$Path=[System.IO.Path]::Combine($PWD.Path, 'Input'); [System.IO.Directory]::CreateDirectory($Path) | Out-Null
 	$Path=[System.IO.Path]::Combine($PWD.Path, 'Output'); [System.IO.Directory]::CreateDirectory($Path) | Out-Null
 	$Path=[System.IO.Path]::Combine($PWD.Path, 'Originals'); [System.IO.Directory]::CreateDirectory($Path) | Out-Null
-	.\ImageMagick\mogrify.exe -path .\Output -format png Input\*.webp Input\*.jpg Input\*.heic Input\*.tiff
-	move-item -Force Input\*.webp, Input\*.jpg, Input\*.heic, Input\*.tiff -Destination .\Originals\
-	}
-}
-
-else {
-	Write-Host "mogrify.exe not found!"
-	Read-Host "Press enter to continue..."
-}
+	.\ImageMagick\magick mogrify -path .\Output -format png Input\*.webp Input\*.jpg Input\*.heic Input\*.tiff
+	move-item -Force Input\*.webp, Input\*.png, Input\*.heic, Input\*.tiff -Destination .\Originals\
--- README.md
+++ README.md
@@ -2,13 +2,10 @@
 Convert images using ImageMagick. To the best of my knowledge, this will only function on Microsoft Windows.
 
 ## Installation
-
-First, install [ImageMagick](https://imagemagick.org/script/download.php#windows). I recommend the version containing "portable-Q16-x64.zip".
-Then, Download the source code above under "code". Click "Download ZIP" to download the source code. Extract the contents anywhere you like.
-Now, extract the contents of ImageMagick zip file into the empty "ImageMagick" folder contained with the script.
-After these steps, the script is now ready for use.
+Download the source code above under "code". Click "Download ZIP" to download the source code. Extract the contents anywhere you like.
 
 ## Usage
+On first run, the script will automatically download and install ImageMagick. Click yes on the Admin prompt if requested. After ImageMagick is installed, it will create the "Input" folder and give you a chance to put in your images.
 Place the images you want to convert in the "Input" folder. Run "Convert Images to JPG.ps1" to convert your photos to JPG, or "Convert Images to PNG.ps1" for PNG conversion.
 The all images of the desired image type will be moved to a folder called "Output", while the original images that have been converted will be moved to a folder called "Originals".
 
